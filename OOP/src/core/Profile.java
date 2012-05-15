d3822c3544c41219c4f2a75fdb3d23aa873a20b0
wrydzewski <wr@web-werker.de>
2012-05-15 17:37:08 +0200

    + sample plugins / parser + parser interface + profile (not finished)

---
+++ OOP/src/core/Profile.java
@@ -0,0 +1,59 @@
+package core;
+
+import java.util.ArrayList;
+
+import core.plugin.bytehawks.Loader;
+
+public class Profile {
+	private ArrayList<Parser> parserList = new ArrayList<Parser>();
+	
+	private void initParser(Loader loader) {
+		ArrayList<String> plugins = loader.getPluginList();
+		System.out.println(plugins);
+		try {
+			for(String plugin : plugins) {
+				Object test = loader.invoke(plugin, "getParser");
+				System.out.println(test);
+				if(test != null) {
+					this.addParser((Parser)test);
+				}
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		/*
+		System.out.println(this.parserList);
+		Parser parser = new RegExParser();
+		for(String key : parser.getParamNames()) {
+			System.out.println(key);
+		}
+		parser.setParam("match", "([0-9])");
+		parser.setParam("replace", "$1$1");
+		
+		System.out.println(parser.parse("test0,12, 8 test"));
+		*/
+	}
+	
+	public void initPlugins() {
+		Loader l = new Loader();
+		l.load();
+		ArrayList<String> plugins = l.getPluginList();
+		
+		initParser(l);
+	}
+	
+	private void addParser(Parser p) {
+		this.parserList.add(p);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Profile p = new Profile();
+		//p.initParser();
+		p.initPlugins();
+	}
+}
---
+++ OOP/src/core/RegExParser.java
@@ -0,0 +1,63 @@
+package core;
+
+import java.util.HashMap;
+import java.util.Set;
+
+import core.plugin.bytehawks.interfaces.Plugin;
+import core.plugin.bytehawks.interfaces.PluginLoader;
+
+
+
+public class RegExParser implements Parser, Plugin {
+
+	private HashMap<String, String> params;
+	
+	public RegExParser() {
+		params = new HashMap<String, String>();
+		params.put("match", "");
+		params.put("replace", "");
+	}
+	
+	@Override
+	public String parse(String input) {
+		try {
+			return input.replaceAll(params.get("match"), params.get("replace"));
+		}
+		catch(Exception e) {}
+		return input;
+	}
+
+	public Set<String> getParamNames() {
+		return params.keySet();
+	}
+	
+	public void setParam(String key, String value) {
+		params.remove(key);
+		params.put(key, value);
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+	@Override
+	public void startPlugin() throws Exception {
+		//
+		
+	}
+
+	@Override
+	public void stopPlugin() throws Exception {
+		// Nothing for now..
+		
+	}
+
+	@Override
+	public void loadPlugin(PluginLoader loader) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	public Parser getParser() {
+		return this;
+	}
+}
